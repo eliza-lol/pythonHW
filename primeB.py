e2ca88e43012abe749a12671b594e7e8ee9fd18b
eliza-lol <73465338+eliza-lol@users.noreply.github.com>
2021-07-06 11:53:37 +0600

    Add files via upload

---
+++ primeB.py
@@ -0,0 +1,10 @@
+def is_prime(n):
+    output=[]
+    for n in range(1,n):
+        if n>1:
+            for i in range(2,n):
+                if n%i==0:
+                    break
+            else:
+                output.append(n)
+    return output
